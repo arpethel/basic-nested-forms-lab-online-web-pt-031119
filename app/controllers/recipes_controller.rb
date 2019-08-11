4f9966282614a9cb812300d10b4d4f1a9f7ca13e
Ryan Pethel <arpethel@gmail.com>
2019-08-11 01:41:03 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,6 +18,12 @@ class RecipesController < ApplicationController
     redirect_to recipe
   end
 
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.update(recipe_params)
+    redirect_to recipe
+  end
+
   private
 
   def recipe_params
